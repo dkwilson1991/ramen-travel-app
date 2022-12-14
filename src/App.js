bd25cc75cf20733fffd1234a9baa207f53483cb0
Keita Wilson <dkwilson1991@gmail.com>
2022-12-14 14:54:15 +0900

    search app navbar made

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { CssBaseline, Grid } from "@material-ui/core";
+
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
 
 const App = () => {
   return (
     <div>
+      <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%'}}>
+        <Grid items xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid items xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
       <h1>Hello, World</h1>
+      </>
     </div>
   );
 };
